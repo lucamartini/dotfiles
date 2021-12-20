2927f404af9e150a2270d87579be96d492b2bffc
Martini Luca <l.martini@nwgenergia.it>
2021-12-20 09:31:08 +0100

    add upgrade.sh

--- misc/upgrade.sh
+++ misc/upgrade.sh
@@ -2,9 +2,9 @@ GREEN="\033[1;32m"
 NOCOLOR="\033[0m"
 
 cd ~/.oh-my-zsh/custom/plugins/
-for dir in */     # list directories in the form "/tmp/dirname/"
+for dir in */    
 do
-    dir=${dir%*/}      # remove the trailing "/"
+    dir=${dir%*/}
 	cd $dir
   echo "upgrading ${GREEN}$dir${NOCOLOR}"
 	git pull
@@ -12,12 +12,21 @@ do
 done
 
 cd ~/.oh-my-zsh/custom/themes/
-for dir in */     # list directories in the form "/tmp/dirname/"
+for dir in */   
 do
-    dir=${dir%*/}      # remove the trailing "/"
+    dir=${dir%*/}
 	cd $dir
   echo "upgrading ${GREEN}$dir${NOCOLOR}"
 	git pull
 	cd ..
 done
 
+cd ~/.tmux/plugins
+for dir in */  
+do
+    dir=${dir%*/} 
+	cd $dir
+  echo "upgrading ${GREEN}$dir${NOCOLOR}"
+	git pull
+	cd ..
+done
