dcd0b60866db63712bc2def9aa49a656b6c84a52
Martini Luca <l.martini@nwgenergia.it>
2021-12-20 10:13:45 +0100

    fix colors

--- misc/upgrade.sh
+++ misc/upgrade.sh
@@ -1,32 +1,23 @@
-GREEN="\033[1;32m"
-NOCOLOR="\033[0m"
+#!/bin/bash
 
-cd ~/.oh-my-zsh/custom/plugins/
-for dir in */    
-do
-    dir=${dir%*/}
-	cd $dir
-  echo "upgrading ${GREEN}$dir${NOCOLOR}"
-	git pull
-	cd ..
-done
+function upgrade_lib () {
+  local GREEN=`tput setaf 2`
+  local YELLOW=`tput setaf 3 bold`
+  local NOCOLOR=`tput sgr0`
 
-cd ~/.oh-my-zsh/custom/themes/
-for dir in */   
-do
-    dir=${dir%*/}
-	cd $dir
-  echo "upgrading ${GREEN}$dir${NOCOLOR}"
-	git pull
-	cd ..
-done
+  echo -e "going to $YELLOW$1$NOCOLOR"
+  cd $1
+  for dir in */    
+  do
+      dir=${dir%*/}
+    cd $dir
+    echo "upgrading $GREEN$dir$NOCOLOR"
+    git pull
+    cd ..
+  done
+}
 
-cd ~/.tmux/plugins
-for dir in */  
-do
-    dir=${dir%*/} 
-	cd $dir
-  echo "upgrading ${GREEN}$dir${NOCOLOR}"
-	git pull
-	cd ..
-done
+upgrade_lib ~/.oh-my-zsh/custom/plugins
+upgrade_lib ~/.oh-my-zsh/custom/themes
+upgrade_lib ~/.tmux/plugins
+
