304d5d00004f3c10fd3510970aa6ba60f78e404b
Martini Luca <l.martini@nwgenergia.it>
2021-12-20 12:28:07 +0100

    adding COQ I hope

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -11,14 +11,29 @@ vim.opt.shiftwidth = 2
 
 vim.opt.termguicolors = true
 
-vim.cmd[[colorscheme dracula]]
+vim.cmd [[colorscheme dracula]]
 require('gitsigns').setup()
 require('lualine').setup({
-	options = {
-		theme = 'dracula'
-	}
+    options = {
+        theme = 'dracula'
+    }
 })
 require('telescope').load_extension('fzf')
 
+local lsp = require "lspconfig"
+local coq = require "coq"
+lsp.bashls.setup(coq.lsp_ensure_capabilities())
 
-
+require'nvim-treesitter.configs'.setup {
+  highlight = {
+    enable = true,
+  },
+	incremental_selection = {
+    enable = true,
+  },
+	indent = {
+    enable = true
+  }
+}
+vim.opt.foldmethod='expr'
+vim.opt.foldexpr=vim.fn["nvim_treesitter#foldexpr"]()
--- .config/nvim/plugin/plugins.lua
+++ .config/nvim/plugin/plugins.lua
@@ -1,49 +1,57 @@
 local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
-  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim',
+                                  install_path})
 end
 
 return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+    use 'wbthomason/packer.nvim'
 
-  use 'dracula/vim'
+    use 'dracula/vim'
 
-  use {
-    'lewis6991/gitsigns.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    },
-  }
+    use {
+        'lewis6991/gitsigns.nvim',
+        requires = {'nvim-lua/plenary.nvim'}
+    }
 
-  use {
-    'nvim-lualine/lualine.nvim',
-    requires = {'kyazdani42/nvim-web-devicons', opt = true}
-  }
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = {
+            'kyazdani42/nvim-web-devicons',
+            opt = true
+        }
+    }
 
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {{'nvim-lua/plenary.nvim'}}
+    }
 
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate'
+    }
 
-  use { 'neovim/nvim-lspconfig' }
+    use {'neovim/nvim-lspconfig'}
 
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = ':TSUpdate'
-  }
+    use {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        run = 'make'
+    }
 
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            require("which-key").setup {}
+        end
+    }
 
-  use {
-    "folke/which-key.nvim",
-    config = function()
-      require("which-key").setup {}
+    use {'ms-jpq/coq_nvim'}
+    use {'ms-jpq/coq.artifacts'}
+    use {'ms-jpq/coq.thirdparty'}
+
+    if packer_bootstrap then
+        require('packer').sync()
     end
-  }
-
-  if packer_bootstrap then
-    require('packer').sync()
-  end
 end)
