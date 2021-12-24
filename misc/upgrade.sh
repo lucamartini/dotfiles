29391eba0e14c07dae0cf398b0fbd72ad327049b
Luca Martini <luca.martini82@gmail.com>
2021-12-24 15:11:47 +0100

    remove zsh-nvm

--- misc/upgrade.sh
+++ misc/upgrade.sh
@@ -21,3 +21,8 @@ upgrade_lib ~/.oh-my-zsh/custom/plugins
 upgrade_lib ~/.oh-my-zsh/custom/themes
 upgrade_lib ~/.tmux/plugins
 
+(
+  cd "$NVM_DIR"
+  git fetch --tags origin
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
