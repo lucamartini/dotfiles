ddf1f5f10271e4fce763c05d7f52f25d324c599c
Luca Martini <luca.martini82@gmail.com>
2021-12-27 22:11:34 +0100

    add lvim upgrade line

--- misc/upgrade.sh
+++ misc/upgrade.sh
@@ -27,3 +27,7 @@ upgrade_lib ~/.tmux/plugins
   git fetch --tags origin
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 ) && \. "$NVM_DIR/nvm.sh"
+
+echo "upgrading$YELLOW lvim$NOCOLOR"
+lvim +LvimUpdate +q
+
