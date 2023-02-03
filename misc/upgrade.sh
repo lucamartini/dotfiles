ecc4fe7b6dc346398f5339905922fd645cc69f91
Martini Luca <l.martini@nwgenergia.it>
2023-02-03 22:52:26 +0100

    add delta

--- misc/upgrade.sh
+++ misc/upgrade.sh
@@ -28,6 +28,3 @@ upgrade_lib ~/.tmux/plugins
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
 ) && \. "$NVM_DIR/nvm.sh"
 
-echo "upgrading$YELLOW lvim$NOCOLOR"
-lvim +LvimUpdate +q
-
