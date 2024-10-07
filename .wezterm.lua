7ea470f3330e6dea3d4a851dfb673dbfb3e41f48
Martini Luca <l.martini@nwgenergia.it>
2024-10-07 17:30:36 +0200

    better log

--- .wezterm.lua
+++ .wezterm.lua
@@ -8,6 +8,7 @@ local config = wezterm.config_builder()
 
 -- config.color_scheme = 'Dracula'
 config.color_scheme = 'Everforest Dark Hard (Gogh)'
+-- config.color_scheme = 'Dracula (Gogh)'
 config.font = wezterm.font('JetBrainsMono Nerd Font Propo')
 config.freetype_load_target = 'Light'
 config.font_size = 12.0
