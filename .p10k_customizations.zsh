5e523a77cac57f4e87596168c6ff27ec0a8a6aa9
Martini Luca <l.martini@nwgenergia.it>
2024-10-10 10:41:00 +0200

    sync colors

--- .p10k_customizations.zsh
+++ .p10k_customizations.zsh
@@ -1,3 +1,13 @@
+# The list of segments shown on the left. Fill it with the most important segments.
+typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(
+  # =========================[ Line #1 ]=========================
+  # os_icon                 # os identifier
+  dir                     # current directory
+  vcs                     # git status
+  # =========================[ Line #2 ]=========================
+  newline                 # \n
+  prompt_char             # prompt symbol
+)
 # The list of segments shown on the right. Fill it with less important segments.
 # Right prompt on the last prompt line (where you are typing your commands) gets
 # automatically hidden when the input line reaches it. Right prompt above the
