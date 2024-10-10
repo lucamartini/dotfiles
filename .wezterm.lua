c6e10082856e13122f3b0023413b7d95bbe5dbe2
Martini Luca <l.martini@nwgenergia.it>
2024-10-10 10:16:06 +0200

    trying everforest

--- .wezterm.lua
+++ .wezterm.lua
@@ -7,9 +7,9 @@ local config = wezterm.config_builder()
 -- This is where you actually apply your config choices
 
 -- config.color_scheme = 'Dracula'
--- config.color_scheme = 'Everforest Dark Hard (Gogh)'
+config.color_scheme = 'Everforest Dark (Gogh)'
 -- config.color_scheme = 'Dracula (Gogh)'
-config.color_scheme = 'Dracula (Official)'
+-- config.color_scheme = 'Dracula (Official)'
 config.font = wezterm.font('JetBrainsMono Nerd Font Propo')
 config.freetype_load_target = 'Light'
 config.font_size = 12.0
