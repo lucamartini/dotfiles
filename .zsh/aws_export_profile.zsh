93443ee6ecdbf81f9ebda9a9fec9fdec95788cf2
Martini Luca <l.martini@nwgenergia.it>
2025-08-18 13:59:35 +0200

    remove dependency on npm

--- .zsh/aws_export_profile.zsh
+++ .zsh/aws_export_profile.zsh
@@ -11,6 +11,7 @@ aws_export_profile() {
     fi
 
     local profile="$1"
+    local credential_file="$HOME/.aws/credentials"
     local section
     [[ "$profile" == "default" ]] && section="default" || section="profile $profile"
 
@@ -24,21 +25,36 @@ aws_export_profile() {
         return 3
     }
 
-    aws-utils copy-credentials --profile "$profile" || {
-        echo "Failed to copy credentials for profile '$profile'"
-        return 4
-    }
+    local credentials
+    credentials=$(aws configure export-credentials --profile "$profile" --output json)
 
-  export AWS_PROFILE="$profile"
-  echo "AWS_PROFILE set to '$profile'"
+    # Extract specific credentials
+    local aws_access_key_id=$(echo "$credentials" | jq -r '.AccessKeyId')
+    local aws_secret_access_key=$(echo "$credentials" | jq -r '.SecretAccessKey')
+    local aws_session_token=$(echo "$credentials" | jq -r '.SessionToken')
+
+    # Removes the existing block for the specific profile from the credentials file
+    sed -i.bak "/^\[$profile\]/,/^$/d" "$credential_file"
+
+    # Adds the new credentials for the profile
+    {
+        echo "[$profile]"
+        echo "aws_access_key_id = $aws_access_key_id"
+        echo "aws_secret_access_key = $aws_secret_access_key"
+        echo "aws_session_token = $aws_session_token"
+        echo ""
+    } >> "$credential_file"
+
+    export AWS_PROFILE="$profile"
+    echo "AWS_PROFILE set to '$profile'"
 }
 
 # Completion
 _aws_export_profile() {
-  local config_file="${AWS_CONFIG_FILE:-$HOME/.aws/config}"
-  local -a profiles
-  profiles=(${(f)"$(grep '^\[profile ' $config_file | sed 's/^\[profile \(.*\)\]$/\1/')"})
-  [[ -f $config_file && $(grep '^\[default\]' $config_file) ]] && profiles+=("default")
-  _describe 'aws profiles' profiles
+    local config_file="${AWS_CONFIG_FILE:-$HOME/.aws/config}"
+    local -a profiles
+    profiles=(${(f)"$(grep '^\[profile ' $config_file | sed 's/^\[profile \(.*\)\]$/\1/')"})
+    [[ -f $config_file && $(grep '^\[default\]' $config_file) ]] && profiles+=("default")
+    _describe 'aws profiles' profiles
 }
 compdef _aws_export_profile aws_export_profile
