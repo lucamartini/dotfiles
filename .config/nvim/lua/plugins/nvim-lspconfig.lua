f31ee20de58755f198e6704753180a3f8ab0cfdd
Martini Luca <l.martini@nwgenergia.it>
2026-01-15 17:33:41 +0100

    no inlay hints

---
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        enabled = false,
+        -- exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
+      },
+    },
+  },
+}
